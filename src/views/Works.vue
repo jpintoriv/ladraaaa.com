d13aeae9ebc4ce6d77eb39c938bcf87c9db02517
Jorge <jorgepintoriveros@gmail.com>
2022-09-19 15:27:21 -0300

    works

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-
     <div id="works" ref="works">
       <navigator-work
         section="Obra"
@@ -21,12 +20,28 @@
     <div v-if="image.visible">
       <div class="background-gallery" @click="hideGallery" />
       <img
+        id="left_image"
+        :src="resolve_img_url(left_image)"
+        :alt="image.description"
+        height="300px"
+        @click="leftImage"
+      />
+      <img
         id="image"
         :src="resolve_img_url(image.name)"
         :alt="image.description"
         height="600px"
+
+        @keyup.left="leftImage"
       />
-      <div class="gallery-text">{{image.description}}</div>
+      <img
+        id="right_image"
+        :src="resolve_img_url(right_image)"
+        :alt="image.description"
+        height="300px"
+        @click="rightImage"
+      />
+      <div class="gallery-text">{{ image.description }}</div>
     </div>
   </div>
 </template>
@@ -46,22 +61,65 @@ export default {
         { name: "riso.jpg" },
         { name: "simulacro_obra.jpg" },
       ],
+      carousel: [
+        { name: "expogabi_1.jpg" },
+        { name: "expogabi_2.jpg" },
+        { name: "expogabi_3.jpg" },
+        { name: "expogabi_4.jpg" },
+        { name: "expogabi_5.jpg" },
+      ],
       image: {
         visible: false,
         name: "encarnacao.jpg",
         description: "lorem ipsum",
       },
+      left_image: "",
+      right_image: "expogabi_1.jpg",
+      actual_image_index: 0,
     };
   },
   methods: {
-    showGallery: function(index){
+    showGallery: function (index) {
       this.image.name = this.works[index].name;
       this.image.visible = true;
+      this.carousel.push({ name: this.image.name });
     },
-    hideGallery: function (){
+    hideGallery: function () {
       this.image.visible = false;
+      this.carousel.splice(this.carousel.length, 1);
+    },
+    leftImage: function () {
+      let quantity_images = this.carousel.length;
+      this.actual_image_index = (this.actual_image_index - 1) % quantity_images;
+
+      this.image.name = this.carousel[this.actual_image_index];
+      this.left_image =
+        this.carousel[
+          (this.actual_image_index + 1 + quantity_images) % quantity_images
+        ].name;
+      this.right_image =
+        this.carousel[
+          (this.actual_image_index - 1 + quantity_images) % quantity_images
+        ].name;
+    },
+    rightImage: function () {
+      let quantity_images = this.carousel.length;
+      this.actual_image_index = (this.actual_image_index + 1) % quantity_images;
+
+      this.image.name = this.carousel[this.actual_image_index].name;
+      this.left_image =
+        this.carousel[
+          (this.actual_image_index - 1 + quantity_images) % quantity_images
+        ].name;
+      this.right_image =
+        this.carousel[
+          (this.actual_image_index + 1 + quantity_images) % quantity_images
+        ].name;
     },
     resolve_img_url: function (path) {
+      if (path === "") {
+        return "";
+      }
       let images = require.context(
         "../assets/works/",
         false,
@@ -84,7 +142,7 @@ body {
 }
 .background-gallery {
   z-index: 98;
-  background: rgba(0,0,0,0.7);
+  background: rgba(0, 0, 0, 0.7);
   height: 100%;
   width: 100%;
   left: 0;
@@ -100,7 +158,23 @@ body {
   -webkit-transform: translate(-50%, -50%);
   transform: translate(-50%, -50%);
 }
-.gallery-text{
+#left_image {
+  z-index: 100;
+  position: absolute;
+  left: 20%;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+#right_image {
+  z-index: 100;
+  position: absolute;
+  left: 80%;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+.gallery-text {
   z-index: 100;
   color: #03ff00;
   font-size: 12px;
