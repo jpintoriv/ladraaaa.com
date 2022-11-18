eba48ee11ef2a7e7f335aa51957b1e0bfc9dc4f0
Jorge <jorgepintoriveros@gmail.com>
2022-11-18 17:59:02 -0300

    A

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const history = require('connect-history-api-fallback')
-const serveStatic = require("serve-static");
-const path = require('path');
+const express = require("express");
+const path = require("path");
 let app = express();
-require("./src/router/index")(app);
-app.use(history());
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"), (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port);
