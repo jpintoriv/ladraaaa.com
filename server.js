d167b2eda2634adffc7427ab1724555131a7bbb6
Jorge <jorgepintoriveros@gmail.com>
2022-11-18 16:57:44 -0300

    a

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ const serveStatic = require("serve-static");
 const path = require("path");
 let app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-app.get(/.*/,(req,res)=>res.sendFile(path.resolve(__dirname,'public/index.html')))
 const port = process.env.PORT || 3000;
 app.listen(port);
