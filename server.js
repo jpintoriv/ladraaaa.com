dfa6ff4101ac60d47681964dce9adbca2d51d3a2
Jorge <jorgepintoriveros@gmail.com>
2022-11-18 18:12:10 -0300

    AA

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require("express");
+const serveStatic = require("serve-static");
 const path = require("path");
 let app = express();
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./dist", "index.html"), (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
+app.use(serveStatic(path.join(__dirname, "dist")));
+app.route("/*").get(function (req, res) {
+  res.sendFile(path.join(__dirname + "dist/index.html"));
 });
-
 const port = process.env.PORT || 3000;
 app.listen(port);
+module.exports = app;
