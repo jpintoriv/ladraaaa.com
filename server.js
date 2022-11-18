57491f279ab1848d78fa243da39d37818d1ab579
Jorge <jorgepintoriveros@gmail.com>
2022-11-18 18:01:51 -0300

    wuas

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 let app = express();
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html"), (err) => {
+  res.sendFile(path.join(__dirname, "./build", "index.html"), (err) => {
     if (err) {
       console.log(err);
     }
