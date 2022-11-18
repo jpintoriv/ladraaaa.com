6e9fc5f21dee782c164203ee106756242a2d26db
Jorge <jorgepintoriveros@gmail.com>
2022-11-18 17:27:34 -0300

    uwu

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const history = require('connect-history-api-fallback')
 const serveStatic = require("serve-static");
 const path = require('path');
 let app = express();
-require("./src/router/index")(app);
+require("./src/router/index.router")(app);
 app.use(history());
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
