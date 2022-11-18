23d9469965505c55524666116a943ac6f51e2be2
Jorge <jorgepintoriveros@gmail.com>
2022-11-18 16:48:47 -0300

    yapo funciona

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
-app.route('/*')
-    .get(function(req, res) {
-  res.serveStatic(path.join(__dirname + "dist"));
-});
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+let app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+app.get(/.*/,(req,res)=>res.sendFile(path.resolve(__dirname,'public/index.html')))
 const port = process.env.PORT || 3000;
 app.listen(port);
