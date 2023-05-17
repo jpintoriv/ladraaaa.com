67ce39b58cca612077dbc4ce7d5ca3d0e8c5b086
Jorge <jorgepintoriveros@gmail.com>
2023-05-17 18:41:15 -0400

    update arts

--- src/views/Arts.vue
+++ src/views/Arts.vue
@@ -6,57 +6,76 @@
         previous_section="Home"
         previous_url="/index"
       />
-      <div>
-        <DraggableDiv
+      <div class="photos">
+        <img
+          class="image-gallery"
           v-for="(work, index) in works"
-          v-bind:image_="work.name"
-          v-bind:alt="work.name"
-          v-bind:index_="index"
-          v-bind:key="work.index"
+          :src="resolve_img_url(work.name)"
+          height="505px"
+          alt="index"
+          :key="work.index"
           @click="showGallery(index)"
         />
       </div>
     </div>
     <div v-if="image.visible" class="gallery">
-      <div class="background-gallery" @click="hideGallery" />
-      <div id="top_image">
-        <img
-          :src="resolve_img_url(left_image)"
-          :alt="image.description"
-          height="300px"
-          @click="leftImage"
-        />
-      </div>
+      <div class="background-gallery" @mousemove="mouseMove"  @click="clickImage" />
       <div id="image">
         <img
           id="center-image"
           :src="resolve_img_url(image.name)"
           :alt="image.description"
+          @mousemove="mouseMove"
+          @click="clickImage"
         />
       </div>
-      <div id="bottom_image">
-        <img
-          :src="resolve_img_url(right_image)"
-          :alt="image.description"
-          height="300px"
-          @click="rightImage"
-        />
+      <div class="gallery-info-container">
+        <div class="gallery-counter">
+          <img
+            v-for="i in quantity_images"
+            :src="getCircle(i)"
+            :key="i"
+            height="17px"
+            alt=""
+          />
+        </div>
+        <div class="gallery-text">{{ image.title }}</div>
+        <div class="gallery-info" @click="showDescription">
+          <a href="#">info</a>
+        </div>
       </div>
-      <div class="gallery-text">{{ image.description }}</div>
+      <div class="gallery-description" v-show="show_description">
+        <div id="close-description" @click="closeDescription">
+          <img
+            alt="Cerrar"
+            src="@/assets/gallery/cruz_info.png"
+            height="30px"
+          />
+        </div>
+        <div class="text-description">
+          {{ image.description }}
+        </div>
+      </div>
+      <div id="close_image" @click="hideGallery" @mousemove="restoreMouse">
+        <img alt="Cerrar" src="@/assets/gallery/cruz.jpg" height="30px" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import NavigatorWork from "@/components/NavigatorWork.vue";
-import DraggableDiv from "@/components/DraggableDiv";
 
 export default {
   name: "Works",
-  components: { NavigatorWork, DraggableDiv },
+  components: { NavigatorWork },
   data: function () {
     return {
       works: [
+        { name: "encarnacao.jpg" },
+        { name: "pendrive.jpg" },
+        { name: "riso.jpg" },
+        { name: "simulacro_obra.jpg" },
         { name: "encarnacao.jpg" },
         { name: "pendrive.jpg" },
         { name: "riso.jpg" },
@@ -72,24 +91,31 @@ export default {
       image: {
         visible: false,
         name: "encarnacao.jpg",
-        description: "lorem ipsum",
+        title: "Text",
+        description:
+          "021Encarnaçao Aquosa. Zaratán, Lisboa 2021Encarnaçao Aquosa. Zaratán," +
+          "Lisboa 2021Encarnaçao Aquosa. Zaratán, Lisboa 2021Encarnaçao Aquosa. Zaratán, " +
+          "Lisboa 2021 Encarnaçao Aquosa. Zaratán, Lisboa 2021Encarnaçao Aquosa. Zaratán, " +
+          "Lisboa 2021Encarnaçao Aquosa. Zaratán, Lisboa 2021Encarnaçao Aquosa. Zaratán, " +
+          "Lisboa 2021Encarnaçao Aquosa. Zaratán, Lisboa 2021Encarnaçao Aquosa. Zaratán, " +
+          "Lisboa 2021Encarnaçao Aquosa. Zaratán, Lisboa 2021Encarnaçao Aquosa. Zaratán, " +
+          "Lisboa 2021 Encarnaçao ",
       },
-      left_image: "",
-      right_image: "",
       actual_image_index: 0,
+      quantity_images: 0,
+      location_cursor: "",
+      show_description: false,
     };
   },
   methods: {
     keyup: function (event) {
       event.preventDefault();
-      if (event.key === "ArrowLeft" || event.key === "ArrowUp") {
+      if (event.key === "ArrowLeft") {
         event.preventDefault();
-        if (this.left_image) {
-          this.leftImage();
-        }
+        this.leftImage();
         return false;
       }
-      if (event.key === "ArrowRight" || event.key === "ArrowDown") {
+      if (event.key === "ArrowRight") {
         event.preventDefault();
         this.rightImage();
       }
@@ -98,46 +124,73 @@ export default {
     showGallery: function (index) {
       this.carousel.unshift({ name: this.works[index].name });
       this.image.name = this.carousel[0].name;
-      this.right_image = this.carousel[1].name;
-      this.left_image = this.carousel[this.carousel.length - 1].name;
+      this.quantity_images = this.carousel.length;
       this.image.visible = true;
       window.addEventListener("keyup", this.keyup);
     },
     hideGallery: function () {
       this.image.visible = false;
+      this.show_description = false;
       this.carousel.shift();
-      this.left_image = "";
+      this.restoreMouse();
     },
     leftImage: function () {
       let quantity_images = this.carousel.length;
       if (this.actual_image_index === 0) {
         this.actual_image_index = quantity_images - 1;
       } else {
-        this.actual_image_index =
-          (this.actual_image_index - 1) % quantity_images;
+        this.actual_image_index = this.actual_image_index - 1;
       }
       this.image.name = this.carousel[this.actual_image_index].name;
-      if (this.actual_image_index === 0) {
-        this.left_image = this.carousel[quantity_images - 1].name;
-      } else {
-        this.left_image =
-          this.carousel[(this.actual_image_index - 1) % quantity_images].name;
-      }
-      this.right_image =
-        this.carousel[(this.actual_image_index + 1) % quantity_images].name;
     },
     rightImage: function () {
       let quantity_images = this.carousel.length;
-      this.actual_image_index = (this.actual_image_index + 1) % quantity_images;
+      if (this.actual_image_index === quantity_images - 1) {
+        this.actual_image_index = 0;
+      } else {
+        this.actual_image_index = this.actual_image_index + 1;
+      }
       this.image.name = this.carousel[this.actual_image_index].name;
-      this.left_image =
-        this.carousel[
-          (this.actual_image_index - 1 + quantity_images) % quantity_images
-        ].name;
-      this.right_image =
-        this.carousel[
-          (this.actual_image_index + 1 + quantity_images) % quantity_images
-        ].name;
+    },
+    restoreMouse: function () {
+      document.body.style.cursor = "default";
+      this.location_cursor = "";
+    },
+    mouseMove: function (event) {
+      const middle = window.innerWidth / 2;
+      if (event.pageX < middle) {
+        document.body.style.cursor = "w-resize";
+        this.location_cursor = "left";
+      } else {
+        document.body.style.cursor = "e-resize";
+        this.location_cursor = "right";
+      }
+    },
+    clickImage: function () {
+      if (this.location_cursor === "left") {
+        this.leftImage();
+      } else {
+        this.rightImage();
+      }
+    },
+    showDescription: function () {
+      this.show_description = true;
+    },
+    closeDescription: function () {
+      this.show_description = false;
+    },
+    getCircle: function (i) {
+      i = i - 1;
+      let images = require.context(
+        "../assets/gallery/",
+        false,
+        /\.png$|\.jpg$|\.gif/
+      );
+      if (i === this.actual_image_index) {
+        return images(`./${"circulo_relleno.png"}`);
+      } else {
+        return images(`./${"circulo.png"}`);
+      }
     },
     resolve_img_url: function (path) {
       if (path === "") {
@@ -158,14 +211,17 @@ export default {
 @import url("https://fonts.googleapis.com/css2?family=Paprika&display=swap");
 
 body {
-  background-color: #cdbcdf;
+  background-color: #000000;
   margin-top: 66px;
   margin-left: 33px;
   margin-right: 33px;
+  overflow-y: scroll;
+}
+a {
+  color: #03ff00;
 }
 .background-gallery {
-  z-index: 98;
-  background: rgba(0, 0, 0, 0.7);
+  background: rgba(0, 0, 0, 1);
   height: 100%;
   width: 100%;
   left: 0;
@@ -174,42 +230,81 @@ body {
   position: fixed;
 }
 #center-image {
-  max-height: 600px;
-  max-width: 800px;
+  max-height: 640px;
 }
 #image {
-  z-index: 100;
+  z-index: 99;
   position: fixed;
   left: 50%;
   top: 50%;
   -webkit-transform: translate(-50%, -50%);
   transform: translate(-50%, -50%);
 }
-#top_image {
-  z-index: 100;
-  position: fixed;
-  left: 50%;
-  top: -20%;
-  -webkit-transform: translate(-50%, 0);
-  transform: translate(-50%, 0);
-}
-#bottom_image {
-  z-index: 100;
-  position: fixed;
-  left: 50%;
-  bottom: -20%;
-  -webkit-transform: translate(-50%, 0);
-  transform: translate(-50%, 0);
-}
 .gallery-text {
+  z-index: 99;
+  color: #03ff00;
+  font-size: 18px;
+  font-family: "Paprika", regular, serif;
+}
+
+.gallery-info {
+  font-size: 18px;
+}
+
+.gallery-description {
+  z-index: 99;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  font-size: 24px;
+  font-family: "Paprika", regular, serif;
+  color: black;
+  width: 1050px;
+  text-align: justify;
+}
+
+.gallery-info-container {
   z-index: 100;
   color: #03ff00;
-  font-size: 12px;
+  font-size: 17px;
   font-family: "Paprika", regular, serif;
   bottom: 90px;
   left: 50%;
   position: fixed;
   -webkit-transform: translate(-50%, -50%);
   transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: space-between;
+  width: 600px;
+}
+
+.photos {
+  padding-top: 100px;
+  margin-left: 43px;
+  margin-right: 43px;
+  justify-content: space-between;
+  display: flex;
+  flex-wrap: wrap;
+}
+.image-gallery {
+  margin-bottom: 90px;
+}
+
+#close_image {
+  z-index: 100;
+  position: fixed;
+  top: 3%;
+  right: 3%;
+}
+
+#close-description {
+  text-align: start;
+}
+
+.gallery-counter{
+  align-self: center;
+  margin-top: 3px;
 }
 </style>
